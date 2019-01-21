f7ef4e26fc345b89ae994e734c77d49a9c8e974d
rafacastalla <rafapenya96@gmail.com>
2019-01-21 12:46:52 +0100

    Add files via upload

---
+++ ArrayCircular/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include "carray.h"
+
+int main() {
+  std::cout << "Hello World!\n";
+
+  int step = 0;
+  int size = 15;
+  Carray<int> buff(size);
+  buff.print();
+
+  std::cout << "\n";
+  std::cout << "Step " << step << "\n";
+  step++;
+  for(int i = 0; i < buff.size; i++){
+    buff.add(i);
+  }
+  buff.print();
+
+  std::cout << "\n";
+  std::cout << "Step " << step << "\n";
+  step++;
+  for(int i = buff.size; i > 0; i--){
+    buff.add(i);
+  }
+  buff.print();
+
+  std::cout << "\n";
+  std::cout << "Step " << step << "\n";
+  step++;
+  for(int i = buff.size/2; i > 0; i--){
+    buff.insert(i);
+  }
+  buff.print();
+
+  std::cout << "\n";
+  std::cout << "Step " << step << "\n";
+  step++;
+  int r;
+  for(int i = buff.size/2; i > 0; i--){
+    buff.remove(&r);
+  }
+  buff.print();
+
+  std::cout << "\n";
+  std::cout << "Step " << step << "\n";
+  step++;
+  for(int i = 0; i < buff.size; i++){
+    buff.add(i);
+  }
+  buff.print();
+
+  std::cout << "\n";
+  std::cout << "Step " << step << "\n";
+  step++;
+  for(int i = 0; i < buff.size; i++){
+    buff.insert(i);
+  }
+  buff.print();
+
+  std::cout << "\n";
+  std::cout << "Step " << step << "\n";
+  step++;
+  for(int i = 0; i < buff.size; i++){
+    buff.get(&r, i);
+    std::cout << r << " ";
+  }
+  std::cout << "\n";
+
+
+}
